91c75f8b516865c42eb06818654a3973ecc98eaa
andrew farquharson <farquharson.a@gmail.com>
2021-07-30 12:10:53 +0100

    Add custom entity form

--- src/Form/ProductForm.php
+++ src/Form/ProductForm.php
@@ -1,15 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: andrew
- * Date: 2021-07-30
- * Time: 10:59
- */
 
 namespace Drupal\dollop\Form;
 
+use Drupal\Core\Entity\ContentEntityForm;
+use Drupal\Core\Form\FormStateInterface;
 
-class ProductForm
+/**
+ * Form for creating/ editing Product entities.
+ *
+ */
+
+class ProductForm extends ContentEntityForm
 {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function save(array $form, FormStateInterface $form_state) {
+        $entity = &$this->entity;
+
+        $status = parent::save($form, $form_state);
+
+        switch ($status) {
+            case SAVED_NEW:
+                drupal_set_message($this->t('Created the %label Product.', [
+                    '%label' => $entity->label(),
+                ]));
+                break;
+
+            default:
+                drupal_set_message($this->t('Saved the %label Product.', [
+                    '%label' => $entity->label(),
+                ]));
+        }
+        $form_state->setRedirect('entity.product.canonical', ['product' =>$entity->id()]);
+    }
 
 }
